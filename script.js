3f225a0b35d8d539ad809bb0742fa05bd28c0897
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-11 23:14:10 +0530

    Complete API config

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const apiKey = "edcb2b265fbaa6f34b817a1975da4208";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${apiKey}&units=metric`
 
-async function checkWeather(){
-    const response = await fetch(apiUrl)
-    var data = await response.json
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+
+    // API Url and GET HTTPS request
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    const response = await fetch(apiUrl);
+    var data = await response.json();  
+    // This conversion takes time due to lags in the fetch API
+    // hence the surrounding code execution needs to be halted
+
     console.log(data)
+    // Updating data - Should be converted to JSON
+    document.querySelector(".city").innerHTML = data.name + "°C";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp);
+    document.querySelector(".humidity").innerHTML = Math.round(data.main.humidity) + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+
 }
 
-checkWeather();
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
